94846b799dd08724ab8e8af986eddc98cbf1bd03
Bharati <binita@netapp.com>
2017-01-15 20:58:13 +0530

    Edited run.sh

--- setup/vagrant/target/scripts/run.sh
+++ setup/vagrant/target/scripts/run.sh
@@ -7,7 +7,6 @@
 cd /home/vagrant/arachne/
 
 
-rm -rf logs/* test.db nohup.out
-
+rm -rf logs/* test.db nohup.out output.log
 
 sudo nohup java -Djava.net.preferIPv4Stack=true -Xms850m -Xmx850m -cp arachne-0.0.1-SNAPSHOT.jar com.github.binitabharati.arachne.service.Main >> output.log 2>&1 &

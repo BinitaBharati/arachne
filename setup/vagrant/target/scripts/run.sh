3bc9507f431a737d2312a6bcaffa2d8da7f661a6
Bharati <binita@netapp.com>
2017-01-20 15:38:59 +0530

    edited run.sh

--- setup/vagrant/target/scripts/run.sh
+++ setup/vagrant/target/scripts/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 
+#sleep for 5 secs. Wait for all sockets to die.
+sleep 5
+
 
 
 
@@ -9,4 +12,4 @@ cd /home/vagrant/arachne/
 
 rm -rf logs/* test.db nohup.out output.log
 
-sudo nohup java -Djava.net.preferIPv4Stack=true -Xms850m -Xmx850m -cp arachne-0.0.1-SNAPSHOT.jar com.github.binitabharati.arachne.service.Main >> output.log 2>&1 &
+sudo nohup java -Djava.net.preferIPv4Stack=true -Xms1048m -Xmx1048m -cp arachne-0.0.1-SNAPSHOT.jar com.github.binitabharati.arachne.service.Main >> output.log 2>&1 &
